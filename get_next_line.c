14df369ac52b43e1808e13776adbe61f22e515ff
Joana Ferro <jferro@student.42lisboa.com>
2022-02-04 19:14:11 +0000

    bugged clean_save

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,79 @@
 /*   By: jferro <jferro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 14:57:43 by jferro            #+#    #+#             */
-/*   Updated: 2022/02/02 20:24:31 by jferro           ###   ########.fr       */
+/*   Updated: 2022/02/04 17:55:11 by jferro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 #include <string.h>
+
+char	*clean_save(char *save)
+{
+	int		i;
+	int		j;
+	char	*p;
+
+	i = 0;
+	j = 0;
+	while (save[i] != '\n' && save[i] != '\0')
+		i++;
+	j = i;
+	while (save[j] != '\n' && save[j] != '\0')
+		j++;
+	p = (char *)malloc((sizeof(char)) * (j - i));
+	if (!p)
+		return (NULL);
+	if (save[i] != '\0')
+	{
+		j = 0;
+		while (save[++i] != '\n' && save[i] != '\0')
+		{
+			p[j] = save[i];
+		}
+	}
+	else
+		return (save);
+	return (p);
+}
+
+void	copy_string(char *restrict dst, const char *restrict src, int size)
+{
+	int	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] != '\n' && src[i] != '\0' && i < size)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		if (src[i] == '\n')
+		{
+			dst[i] = '\n';
+			i++;
+		}
+		dst[i] = '\0';
+	}
+}
+
+char	*get_line(char	*save)
+{
+	int		i;
+	char	*new_save;
+
+	i = 0;
+	while (save[i] != '\n' && save[i] != '\0')
+		i++;
+	new_save = (char *)malloc((sizeof(char)) * (i + 2));
+	if (!new_save)
+		return (NULL);
+	copy_string(new_save, save, i);
+	return (new_save);
+}
+
 char	*read_and_save(int fd, char *save)
 {
 	char	*buffer;
@@ -43,14 +109,23 @@ char	*read_and_save(int fd, char *save)
 char	*get_next_line(int fd)
 {
 	static char	*save;
+	char		*line;
+
 	save = read_and_save(fd, save);
-	return (save);
+	line = get_line(save);
+	save = clean_save(save);
+	return (line);
 }
 int	main(void)
 {
-	int fd;
-	//char *text;
-	fd = open("text.txt", O_RDONLY);
-	//text = get_next_line(fd);
-	printf("%s\n" ,get_next_line(fd));
+	// printf("Insert input: \n");
+	// int fd = 0;
+	// printf("You inserted = %s", get_next_line(fd));
+
+	int fd = open("text.txt", O_RDONLY);
+	//char *line = get_next_line(fd);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,22 @@
 /*   By: jferro <jferro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 18:13:16 by jferro            #+#    #+#             */
-/*   Updated: 2022/02/02 20:13:35 by jferro           ###   ########.fr       */
+/*   Updated: 2022/02/04 17:15:22 by jferro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#define BUFFER_SIZE 100
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+//# define BUFFER_SIZE 1
 
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1);
+
+#endif
